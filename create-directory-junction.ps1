bbc2ca3ee49214a0ae6c3725eace933c90e15df0
jongbin oh <ohyecloudy@gmail.com>
2012-09-08 14:21:27 +0900

    dead space 2 추가, directory junction 설정 추가

--- create-directory-junction.ps1
+++ create-directory-junction.ps1
@@ -1,4 +1,5 @@
 ﻿$my_documents = [Environment]::GetFolderPath("MyDocuments")
 
 cmd /c mklink /J "$my_documents/Electronic Arts" "$my_documents/saved-game-files/Electronic Arts"
+cmd /c mklink /J "$my_documents/EA Games" "$my_documents/saved-game-files/EA Games"
 
